fdd592f704ce55792e018e42ddd139cf72a52eab
konmayu <adenosine.nem@gmail.com>
2023-03-30 01:13:53 +0900

    mod int.py

--- datadog_status_int.py
+++ datadog_status_int.py
@@ -9,6 +9,9 @@ INTERVAL_SECONDS = 60 * 5  # 5分ごとにチェック
 WEBHOOK_URL = "https://hooks.slack.com/services/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"  # 通知を送信するWebhook URL
 LOG_FILE_PATH = "/root/develop/python/excute.log"  # ログファイルのパスを指定
 
+error_count = 0 #except処理のエラー回数初期値を定義
+max_errors = 3  #except処理のエラー回数最大値を定義
+
 def send_notification(message):
     payload = {"text": message}
     requests.post(WEBHOOK_URL, json=payload)
@@ -34,6 +37,11 @@ def main():
                             print(f"Notification sent at {datetime.now()}")
                 except Exception as e:
                     print(f"Error while checking feed: {e}")
+                    error_count += 1
+                    if error_count >= max_errors:
+                        send_notification("Error count reached the maximum limit. Please check the script.")
+                        print("Too many errors, exiting...")
+                        break
 
                 last_checked_time = datetime.utcnow()
                 time.sleep(INTERVAL_SECONDS)
