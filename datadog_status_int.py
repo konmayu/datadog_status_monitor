a76391640f317423a81b11039c0c352a542933ab
konmayu <adenosine.nem@gmail.com>
2023-03-30 01:11:38 +0900

    first commit

---
+++ datadog_status_int.py
@@ -0,0 +1,44 @@
+import time
+import feedparser
+import requests
+import sys
+from datetime import datetime
+
+DATADOG_STATUS_FEED_URL = "https://status.datadoghq.com/history.rss"
+INTERVAL_SECONDS = 60 * 5  # 5分ごとにチェック
+WEBHOOK_URL = "https://hooks.slack.com/services/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"  # 通知を送信するWebhook URL
+LOG_FILE_PATH = "/root/develop/python/excute.log"  # ログファイルのパスを指定
+
+def send_notification(message):
+    payload = {"text": message}
+    requests.post(WEBHOOK_URL, json=payload)
+
+def main():
+    with open(LOG_FILE_PATH, "a") as f:
+        original_stdout = sys.stdout
+        try:
+            sys.stdout = f  # 標準出力をログファイルにリダイレクト
+            print("Script started")
+
+            last_checked_time = datetime.utcnow()
+            print(last_checked_time)
+
+            while True:
+                try:
+                    feed = feedparser.parse(DATADOG_STATUS_FEED_URL)
+                    for entry in feed.entries:
+                        entry_time = datetime.fromtimestamp(time.mktime(entry.published_parsed))
+                        print(entry_time)
+                        if entry_time > last_checked_time:
+                            send_notification(f"{entry_time} : Datadog Status Update: {entry.title} - {entry.link}")
+                            print(f"Notification sent at {datetime.now()}")
+                except Exception as e:
+                    print(f"Error while checking feed: {e}")
+
+                last_checked_time = datetime.utcnow()
+                time.sleep(INTERVAL_SECONDS)
+        finally:
+            sys.stdout = original_stdout
+
+if __name__ == "__main__":
+    main()
