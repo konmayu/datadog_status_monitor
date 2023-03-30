aa2b3b2efc74d979301380bab0ebda7cc505e441
konmayu <adenosine.nem@gmail.com>
2023-03-30 23:56:14 +0900

    mod datadog_status_int.py

--- datadog_status_int.py
+++ datadog_status_int.py
@@ -1,3 +1,4 @@
+import os
 import time
 import feedparser
 import requests
@@ -7,7 +8,7 @@ from datetime import datetime
 DATADOG_STATUS_FEED_URL = "https://status.datadoghq.com/history.rss"
 INTERVAL_SECONDS = 60 * 5  # 5分ごとにチェック
 WEBHOOK_URL = "https://hooks.slack.com/services/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"  # 通知を送信するWebhook URL
-LOG_FILE_PATH = "/root/develop/python/excute.log"  # ログファイルのパスを指定
+#LOG_FILE_PATH = "/root/develop/python/excute.log"  # ログファイルのパスを指定
 
 error_count = 0 #except処理のエラー回数初期値を定義
 max_errors = 3  #except処理のエラー回数最大値を定義
@@ -17,11 +18,19 @@ def send_notification(message):
     requests.post(WEBHOOK_URL, json=payload)
 
 def main():
-    with open(LOG_FILE_PATH, "a") as f:
+    # 出力するログファイルの設定を記述
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    current_date = datetime.now().strftime("%y%m%d_%S")
+    log_file_name = f"excute_{current_date}.log"
+    log_file_path = os.path.join(script_dir, log_file_name)
+
+    with open(log_file_path, "a") as f:
         original_stdout = sys.stdout
         try:
             sys.stdout = f  # 標準出力をログファイルにリダイレクト
             print("Script started")
+            #スクリプト実行開始を通知する
+            send_notification(f"script is started, LogFileName is {log_file_name} and path {log_file_path}")
 
             last_checked_time = datetime.utcnow()
             print(last_checked_time)
@@ -33,6 +42,7 @@ def main():
                         entry_time = datetime.fromtimestamp(time.mktime(entry.published_parsed))
                         print(entry_time)
                         if entry_time > last_checked_time:
+                       # if entry_time < last_checked_time:
                             send_notification(f"{entry_time} : Datadog Status Update: {entry.title} - {entry.link}")
                             print(f"Notification sent at {datetime.now()}")
                 except Exception as e:
@@ -50,3 +60,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
